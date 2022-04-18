0b33747c9f3e0d225f59edcf09b77b63b6742846
Minh Phan <pnminh1710@gmail.com>
2022-04-19 06:54:55 +0700

    Update

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,9 +24,9 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  // workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -41,56 +41,56 @@ const config: PlaywrightTestConfig = {
   },
 
   /* Configure projects for major browsers */
-  // projects: [
-  //   {
-  //     name: "chromium",
-  //     use: {
-  //       ...devices["Desktop Chrome"],
-  //     },
-  //   },
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
 
-  //   {
-  //     name: "firefox",
-  //     use: {
-  //       ...devices["Desktop Firefox"],
-  //     },
-  //   },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+      },
+    },
 
-  //   {
-  //     name: "webkit",
-  //     use: {
-  //       ...devices["Desktop Safari"],
-  //     },
-  //   },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+      },
+    },
 
-  /* Test against mobile viewports. */
-  // {
-  //   name: 'Mobile Chrome',
-  //   use: {
-  //     ...devices['Pixel 5'],
-  //   },
-  // },
-  // {
-  //   name: 'Mobile Safari',
-  //   use: {
-  //     ...devices['iPhone 12'],
-  //   },
-  // },
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: {
+    //     ...devices['Pixel 5'],
+    //   },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: {
+    //     ...devices['iPhone 12'],
+    //   },
+    // },
 
-  /* Test against branded browsers. */
-  // {
-  //   name: 'Microsoft Edge',
-  //   use: {
-  //     channel: 'msedge',
-  //   },
-  // },
-  // {
-  //   name: 'Google Chrome',
-  //   use: {
-  //     channel: 'chrome',
-  //   },
-  // },
-  // ],
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: {
+    //     channel: 'msedge',
+    //   },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: {
+    //     channel: 'chrome',
+    //   },
+    // },
+  ],
 
   /* Folder for test artifacts such as screenshots, videos, traces, etc. */
   // outputDir: 'test-results/',
